eb27f42728560b0ba9bc6a0a752bb7ea7852965c
Ricardo Alfonso Camayo Erazo <ricardo.alfonso.camayo@gmail.com>
2025-10-26 01:57:37 -0500

    Implement smart pointer examples (unique_ptr, shared_ptr, weak_ptr) with Weapon, Texture, Material, Player, and Enemy classes for memory management demonstration

--- 0x00-C++Modern-Review_Unreal_Basics/SmartPointers.cpp
+++ 0x00-C++Modern-Review_Unreal_Basics/SmartPointers.cpp
@@ -1,87 +1,47 @@
 #include "SmartPointers.h"
 
-class Weapon
-{
-    public:
-        virtual ~Weapon() = {std::cout << "Weapon destroyed\n"; };
-        virtual void Fire() = 0;
-};
+/**
+    * Implementations for Weapon, Texture, Material, Player, and Enemy classes.
+ */
+Weapon::~Weapon() {
+    std::cout << "Weapon destroyed\n";
+}
 
-class Pistol : public Weapon
-{
-    public:
-        void Fire() override
-        {
-            std::cout << "Bang!\n";
-        }
-};
+void Pistol::Fire() {
+    std::cout << "Bang!\n";
+}
 
-class Rifle : public Weapon
-{
-    public:
-        void Fire() override
-        {
-            std::cout << "Rat-a-tat-tat!\n";
-        }
-};
+void Rifle::Fire() {
+    std::cout << "Rat-a-tat-tat!\n";
+}
 
-
-std::unique_ptr<Weapon> CreateUniqueWeapon(const std::string& type)
-{
-    if (type == "pistol")
-    {
+std::unique_ptr<Weapon> CreateUniqueWeapon(const std::string& type) {
+    if (type == "pistol") {
         return std::make_unique<Pistol>();
-    }
-    else if (type == "rifle")
-    {
+    } else if (type == "rifle") {
         return std::make_unique<Rifle>();
     }
     return nullptr;
 }
 
-class Texture
-{
-    public:
-        Texture(const std::string& name) : name_(name)
-        {
-            std::cout << "Texture loaded " << name_ << "\n";
-        }
-        ~Texture()
-        {
-            std::cout << "Texture unloaded: " << name_ << "\n";
-        }
-    private:
-        std::string name_;
-};
+Texture::Texture(const std::string& name) : name_(name) {
+    std::cout << "Texture loaded: " << name_ << "\n";
+}
 
-class Material
-{
-    public:
-        Material(std::shared_ptr<Texture> tex) : texture_(tex) {}
-    private:
-        std::shared_ptr<Texture> texture_;
-};
+Texture::~Texture() {
+    std::cout << "Texture unloaded: " << name_ << "\n";
+}
 
-class  Enemy;
+Material::Material(std::shared_ptr<Texture> tex) : texture_(tex) {}
 
-class Player {
-    public:
-        void SetTarget(std::shared_ptr<Enemy> enemy);
-        void Attack();
-    private:
-        std::weak_ptr<Enemy> target_;
-};
+void Enemy::TakeDamage(int damage) {
+    health_ -= damage;
+    std::cout << "Enemy health: " << health_ << "\n";
+}
 
-class Enemy : public std::enable_shared_from_this<Enemy> {
-public:
-    void TakeDamage(int damage) {
-        health_ -= damage;
-        std::cout << "Enemy health: " << health_ << "\n";
-    }
-    bool IsAlive() const { return health_ > 0; }
-private:
-    int health_ = 100;
-};
+bool Enemy::IsAlive() const {
+    return health_ > 0;
+}
 
 void Player::SetTarget(std::shared_ptr<Enemy> enemy) {
     target_ = enemy;
--- 0x00-C++Modern-Review_Unreal_Basics/main.cpp
+++ 0x00-C++Modern-Review_Unreal_Basics/main.cpp
@@ -1,5 +1,9 @@
 #include "SmartPointers.h"
 
+/**
+    * Main function to test Smart Pointer implementations.
+    * Demonstrates unique_ptr, shared_ptr, and weak_ptr usage.
+ */
 int main()
 {
     std::cout << "=== Test Unique Ptr ===\n";
@@ -7,4 +11,23 @@ int main()
         auto weapon = CreateUniqueWeapon("pistol");
         weapon->Fire();
     }
+
+    std::cout << "\n=== Test Shared Ptr ===\n";
+    {
+        auto texture = std::make_shared<Texture>("brick.png");
+        Material mat1(texture);
+        Material mat2(texture);
+    }
+
+    std::cout << "\n=== Test Weak Ptr ===\n";
+    Player player;
+    {
+        auto enemy = std::make_shared<Enemy>();
+        player.SetTarget(enemy);
+        player.Attack();
+        player.Attack();
+    }
+    player.Attack();
+
+    return 0;
 }
